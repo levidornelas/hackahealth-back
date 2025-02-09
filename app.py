bcee8f1ee1d2fd1720e694d0ff70a643e12c919e
Levi Dornelas <levidornelas2006@gmail.com>
2025-02-09 11:14:09 -0300

    fix: deploy

--- app.py
+++ app.py
@@ -183,9 +183,3 @@ async def get_dashboard_paciente_y():
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
-import uvicorn
-import os
-uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
-
-    
